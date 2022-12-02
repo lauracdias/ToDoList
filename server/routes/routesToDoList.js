a87ac9c83fb77e82c04e4f414c9ab58de8f3ae3c
Laura Dias <laura.c@gec.inatel.br>
2022-12-01 21:32:36 -0300

    feat: :sparkles: Add validation

--- server/routes/routesToDoList.js
+++ server/routes/routesToDoList.js
@@ -1,17 +1,32 @@
 const router = require('express').Router()
 const { rawListeners } = require('../models/itensToDoList')
 const toDoItensModel = require('../models/itensToDoList')
+const validate = require('validate.js');
+const Utils = require('../utils/utils');
+const Constants = require('../utils/constants');
+const Constraints = require('../utils/item_validation');
+const Validation = require('../utils/validation');
 
 // adicionar novo item na lista
 router.post('/api/item', async (req, res) => {
     try
-    {
+    {   
         const novoItem = new toDoItensModel(
             {
                 item: req.body.item
             }
         )
+
+        const validation = Validation.create(novoItem);
+        if (validation) {
+            return validation;
+        }
+
         const salvarItem = await novoItem.save()
+        if (salvarItem.code === 400) {
+            const result = Constants.ErrorValidation;
+            return result;
+        }
         res.status(200).json(salvarItem)
     }
     catch(err)
@@ -37,7 +52,21 @@ router.get('/api/items', async (req, res) => {
 router.put('/api/item/:id', async (req, res) => {
     try
     {
+        const itemAtualizado = new toDoItensModel(
+            {
+                item: req.body.item
+            }
+        )
+
+        const validation = Validation.update(itemAtualizado);
+        if (validation) {
+            return validation;
+        }
         const atualizarItem = await toDoItensModel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        if (atualizarItem.code === 400) {
+            const result = Constants.ErrorValidation;
+            return result;
+        }
         res.status(200).json(atualizarItem)
     }
     catch(err)
@@ -53,7 +82,7 @@ router.delete('/api/item/:id', async (req, res) => {
     {
         const apagarItem = await toDoItensModel.findByIdAndDelete(req.params.id)
         req.status(200).json('Item deletado!')
-        res.status(200).json(todosItens)
+        res.status(200).json(apagarItem)
     }
     catch(err)
     {
---
+++ server/utils/utils.js
@@ -0,0 +1,26 @@
+const { StatusCodes } = require('http-status-codes');
+
+const Constants = require('./constants');
+
+const Utils = {
+    responseStatus(data) {
+        switch (data) {
+            case undefined:
+                return StatusCodes.OK;
+            case Constants.ErrorValidation.name:
+                return StatusCodes.BAD_REQUEST;
+            case Constants.ErrorDuplicate.name:
+                return StatusCodes.CONFLICT;
+            case Constants.ErrorNotFound.name:
+                return StatusCodes.NOT_FOUND;
+            default:
+                return StatusCodes.INTERNAL_SERVER_ERROR;
+        }
+    },
+
+    generateUuid(){
+        return uuidv4();
+    },
+};
+
+module.exports = Utils;
